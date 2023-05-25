65901b3a462647b842db3552cbe3873993069b9b
ObiOneKenobii <jimijnrrruji@gmail.com>
2023-05-25 14:50:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+<script>
+    $(document).ready(function() {
+      $('.brand').addClass('animate__animated animate__fadeInLeft');
+      $('.header-content h1').addClass('animate__animated animate__bounceInDown');
+      $('.header-content p').addClass('animate__animated animate__fadeInUp animate__delay-1s'); }
